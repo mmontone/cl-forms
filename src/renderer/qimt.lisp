cefda1685e3b6f37c32f395dbfdd98fe7fe5f67c
mmontone <marianomontone@gmail.com>
2014-10-17 13:55:06 -0300

    QIMT test

--- src/renderer/qimt.lisp
+++ src/renderer/qimt.lisp
@@ -67,3 +67,63 @@
 		       (<selected= "selected"))
 		      (xd (funcall (forms::field-formatter field)
 				   choice)))))))
+
+(defmethod forms::renderer-render-field-widget
+    ((renderer (eql :qimt))
+     (field forms::choice-form-field) form &rest args)
+  (cond
+    ((and (forms::field-expanded field)
+	  (forms::field-multiple field))
+     (let ((selected-keys (mapcar #'first (forms::field-keys-and-values field))))
+       ;; Render checkboxes
+       (loop for (key . choice) in (forms::field-choices-alist field)
+	  do
+	    (<input (<type= "checkbox")
+		    (<name= (forms::form-field-name field form))
+		    (<value= key)
+		    (when (member key selected-keys)
+		      (<checked= "checked"))
+		    (xd (funcall (forms::field-formatter field)
+				 choice))))))
+    ((and (forms::field-expanded field)
+	  (not (forms::field-multiple field)))
+     ;; Render radio buttons
+     (let ((selected-value (forms::field-key-and-value field)))
+       (loop for (key . choice) in (forms::field-choices-alist field)
+	  do
+	    (<input (<type= "radio")
+		    (<name= (forms::form-field-name field form))
+		    (<value= (princ-to-string key))
+		    (when (equalp (first selected-value)
+				  key)
+		      (<checked= "checked"))
+		    (xd (funcall (forms::field-formatter field)
+				 choice))))))
+    ((and (not (forms::field-expanded field))
+	  (forms::field-multiple field))
+     ;; A multiple select box
+     (let ((selected-keys (mapcar #'first (forms::field-keys-and-values field))))
+       (<select
+	 (<name= (forms::form-field-name field form))
+	 (<multiple= "multiple")
+	 (loop for (key . choice) in (forms::field-choices-alist field)
+	    do
+	      (<option (<value= (princ-to-string key))
+		       (when (member key selected-keys)
+			 (<selected= "selected"))
+		       (xd (funcall (forms::field-formatter field)
+				    choice)))))))
+    ((and (not (forms::field-expanded field))
+	  (not (forms::field-multiple field)))
+     ;; A single select box
+     (let ((selected-value (forms::field-key-and-value field)))
+       (<select
+	 (<name= (forms::form-field-name field form))
+	 (loop for (key . choice) in (forms::field-choices-alist field)
+	    do
+	      (<option (<value= (princ-to-string key))
+		       (when (equalp (first selected-value)
+				     key)
+			 (<selected= "selected"))
+		       (xd (funcall (forms::field-formatter field)
+				    choice)))))))))
---
+++ test/qimt.lisp
@@ -0,0 +1,128 @@
+(defpackage :forms.test.qimt
+  (:use :cl :forms :qimt :html))
+
+(in-package :forms.test.qimt)
+
+(defmacro with-html (&body body)
+  (alexandria:with-unique-names (html)
+    `(with-output-to-string (,html)
+       (qimt:serialize-html 
+	(qimt:document
+	  ,@body)
+	,html))))
+
+(hunchentoot:start (make-instance 'hunchentoot:easy-acceptor :port 2021))
+
+(forms:defform simple-form (:action "/qimt/simple-form/post")
+  ((name :string :value "")
+   (ready :boolean :value t)
+   (sex :choice :choices (list "Male" "Female") :value "Male")
+   (submit :submit :label "Create")))
+
+(hunchentoot:define-easy-handler (simple-form :uri "/qimt/simple-form") ()
+  (let ((form (forms::get-form 'simple-form)))
+    (with-html
+      (forms:with-form-renderer :qimt
+	(forms:render-form form)))))
+
+(hunchentoot:define-easy-handler (simple-form-post :uri "/qimt/simple-form/post" :default-request-type :post) ()
+  (let ((form (forms:get-form 'simple-form)))
+    (forms::handle-request form)
+    (forms::validate-form form)
+    (forms::with-form-fields (name ready sex) form
+      (with-html
+      	(<ul 
+	 (<li (fxd "Name: ~A" (forms::field-value name)))
+	 (<li (fxd "Ready: ~A" (forms::field-value ready)))
+	 (<li (fxd "Sex: ~A" (forms::field-value sex))))))))
+
+(defclass person ()
+  ((name :initarg :name
+	 :accessor person-name
+	 :initform nil)
+   (single :initarg :single
+	   :accessor person-single
+	   :initform t)
+   (sex :initarg :sex
+	:accessor person-sex
+	:initform :male)))
+
+(forms:defform-builder model-form (person)
+  (make-instance 'forms::form
+		 :name 'model-form
+		 :model person
+		 :action "/model-form/post"
+		 :fields (forms::make-form-fields
+			  `((name :string :label "Name"
+				  :accessor person-name)
+			    (single :boolean :label "Single"
+				    :accessor person-single)
+			    (sex :choice :label "Sex"
+				 :choices (:male :female)
+				 :accessor person-sex
+				 :formatter format-sex)
+			    (submit :submit :label "Update")))))
+
+(defun format-sex (sex)
+  (if (equalp sex :male) "Male" "Female"))
+
+(hunchentoot:define-easy-handler (model-form :uri "/qimt/model-form") ()
+  (let ((person (make-instance 'person
+			       :name "Foo"
+			       :single t
+			       :sex :male)))
+    (let ((form (forms::get-form 'model-form person)))
+      (with-html
+	(forms:with-form-renderer :qimt
+	  (forms:render-form form))))))
+
+(hunchentoot:define-easy-handler (model-form-post :uri "/model-form/post"
+						  :default-request-type :post) ()
+  (let ((person (make-instance 'person)))
+    (let ((form (forms:get-form 'model-form person)))
+      (forms::handle-request form)
+      (forms::validate-form form)
+      (with-html
+	(<ul
+	 (<li (fxd "Name: ~A" (person-name person)))
+	 (<li (fxd "Single: ~A" (person-single person)))
+	 (<li (fxd "Sex: ~A" (person-sex person))))))))
+
+;; Choices widget test
+
+(forms:defform choices-form (:action "/qimt/choices-form/post")
+  ((sex :choice
+	:choices (list "Male" "Female")
+	:value "Male")
+   (sex2 :choice
+	 :choices (list "Male" "Female")
+	 :value "Female"
+	 :expanded t)
+   (choices :choice
+	    :choices (list "Foo" "Bar")
+	    :value (list "Foo")
+	    :multiple t)
+   (choices2 :choice
+	     :choices (list "Foo" "Bar")
+	     :value (list "Bar")
+	     :multiple t
+	     :expanded  t)
+   (submit :submit :label "Ok")))
+
+(hunchentoot:define-easy-handler (choices-form :uri "/qimt/choices-form") ()
+  (let ((form (forms::get-form 'choices-form)))
+    (with-html
+      (forms:with-form-renderer :qimt
+	(forms:render-form form)))))
+
+(hunchentoot:define-easy-handler (choices-form-post :uri "/qimt/choices-form/post" :default-request-type :post) ()
+  (let ((form (forms:get-form 'choices-form)))
+    (forms::handle-request form)
+    (forms::validate-form form)
+    (forms::with-form-field-values (sex sex2 choices choices2) form
+      (with-html
+	(<ul 
+	 (<li (fxd "Sex: ~A" sex))
+	 (<li (fxd "Sex2: ~A" sex2))
+	 (<li (fxd "Choices: ~A" choices))
+	 (<li (fxd "Choices2: ~A" choices2)))))))
