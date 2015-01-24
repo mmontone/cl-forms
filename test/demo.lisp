f6790ecde0a074c61e2463e5d826ffe10a3ad7a1
Mariano Montone <marianomontone@gmail.com>
2015-01-24 11:22:30 -0300

    Demo: pretty print source

--- test/demo.lisp
+++ test/demo.lisp
@@ -8,30 +8,40 @@
       (read-sequence data stream)
       data)))
 
+(defun ensure-file (path)
+  (when (not (probe-file path))
+    (error "File not found: ~A" path))
+  path)
 
 (defparameter *demo-acceptor*
   (make-instance 'hunchentoot:easy-acceptor :port 9090))
 
-(push
- (hunchentoot:create-static-file-dispatcher-and-handler
-  "/bootstrap.css"
-  (asdf:system-relative-pathname :cl-forms
-                                 "test/static/bower_components/bootstrap/dist/css/bootstrap.min.css"))
- hunchentoot:*dispatch-table*)
+(defun define-static-resource (uri relative-path)
+  (push
+   (hunchentoot:create-static-file-dispatcher-and-handler
+    uri
+    (ensure-file
+     (asdf:system-relative-pathname :cl-forms relative-path)))
+   hunchentoot:*dispatch-table*)
+  uri)
 
-(push
- (hunchentoot:create-static-file-dispatcher-and-handler
-  "/bootstrap.js"
-  (asdf:system-relative-pathname :cl-forms
-                                 "test/static/bower_components/bootstrap/dist/js/bootstrap.min.js"))
- hunchentoot:*dispatch-table*)
+(define-static-resource "/bootstrap.css"
+    "test/static/bower_components/bootstrap/dist/css/bootstrap.min.css")
 
-(push
- (hunchentoot:create-static-file-dispatcher-and-handler
-  "/jquery.js"
-  (asdf:system-relative-pathname :cl-forms
-                                 "test/static/bower_components/jquery/dist/jquery.min.js"))
- hunchentoot:*dispatch-table*)
+(define-static-resource "/bootstrap.js"
+    "test/static/bower_components/bootstrap/dist/js/bootstrap.min.js")
+
+(define-static-resource "/jquery.js"
+    "test/static/bower_components/jquery/dist/jquery.min.js")
+
+(define-static-resource "/prettify.js"
+    "test/static/prettify/prettify.js")
+
+(define-static-resource "/lang-lisp.js"
+    "test/static/prettify/lang-lisp.js")
+
+(define-static-resource "/prettify.css"
+    "test/static/prettify/prettify.css")
 
 (defun start-demo ()
   (hunchentoot:start *demo-acceptor*))
@@ -40,18 +50,24 @@
   (hunchentoot:stop *demo-acceptor*))
 
 (defun render-demo-page (&key (demo (error "Provide the demo"))
-                              (source (error "Provide the source"))
-                              (active-menu :fields))
+                           (source (error "Provide the source"))
+                           (active-menu :fields))
   (who:with-html-output-to-string (forms.who::*html*)
     (:html
      (:head
       (:title "cl-forms demo")
-      (:link :rel "stylesheet" :href "bootstrap.css"))
-     (:body
+      (:link :rel "stylesheet" :href "/bootstrap.css")
+      (:link :rel "stylesheet" :href "/prettify.css")
+      (:script :src "/jquery.js")
+      (:script :src "/bootstrap.js")
+      (:script :src "/prettify.js")
+      (:script :src "/lang-lisp.js"))
+     (:body :onload "prettyPrint()"
       (:header :role"banner" :id "top" :class "navbar navbar-static-top"
                (:div :class "container"
                      (:div :class "navbar-header"
-                           (:a :class "navbar-brand" :href "/" (who:str "CL-FORMS demo")))))
+                           (:a :class "navbar-brand" 
+			       :href "/" (who:str "CL-FORMS demo")))))
       (:div :class "container"
             (:div :class "col-md-3"
                   (:ul :class "nav nav-pills nav-stacked"
@@ -59,7 +75,7 @@
                             :class (and (equalp active-menu :fields)
                                         "active")
                             (:a :href "/fields" (who:str "Fields")))
-		       (:li :role "presentation"
+                       (:li :role "presentation"
                             :class (and (equalp active-menu :models)
                                         "active")
                             (:a :href "/models" (who:str "Models")))
@@ -81,26 +97,28 @@
                             (:a :href "/tests" (who:str "Tests")))))
             (:div :role "tabpanel" :class "col-md-9"
                   (:ul :class "nav nav-tabs" :role "tablist"
-		       (:li :role "tab" :class "active"
+                       (:li :role "tab" :class "active"
                             :aria-controls "demo"
                             (:a :href "#demo" :data-toggle "tab"
-				(who:str "Demo")))
+                                (who:str "Demo")))
                        (:li :role "tab"
                             :aria-controls "source"
                             (:a :href "#source" :data-toggle "tab"
 				(who:str "Source"))))
                   (:div :class "tab-content"
-                        (:div :role "tabpanel" :class "tab-pane active" :id "demo"
+                        (:div :role "tabpanel" :class "tab-pane active" 
+			      :id "demo"
                               (funcall demo))
-                        (:div :role "tabpanel" :class "tab-pane" :id "source"
-                              (:div :class "prettyprint"
-				    (who:str (file-string source)))))))
-                                        ;(:script :src "https://google-code-prettify.googlecode.com/svn/loader/run_prettify.js")
-      (:script :src "jquery.js")
-      (:script :src "bootstrap.js")))))
+                        (:div :role "tabpanel" :class "tab-pane" 
+			      :id "source"
+                              (:pre :class "prettyprint"
+                                    (who:str
+                                     (file-string source)
+                                     ))))))
+      ))))
 
 (hunchentoot:define-easy-handler (demo-main :uri "/") ()
   (render-demo-page :demo #'fields-demo
-		    :source (asdf:system-relative-pathname :cl-forms.demo 
-							   "test/demo/fields.lisp")
-		    :active-menu :fields))
+                    :source (asdf:system-relative-pathname :cl-forms.demo
+                                                           "test/demo/fields.lisp")
+                    :active-menu :fields))
