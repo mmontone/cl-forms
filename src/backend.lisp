fc015ddd63750fce467cfaa1b7b2753175596fc7
Mariano Montone <marianomontone@gmail.com>
2025-01-06 20:54:31 -0300

    Not used

--- src/backend.lisp
+++
@@ -1,6 +0,0 @@
-(in-package :cl-forms)
-
-(defvar *backend*) ;; The REQUEST backend implementation
-
-(defgeneric backend-handle-request (form request)
-  (:documentation "Handle REQUEST for FORM"))
