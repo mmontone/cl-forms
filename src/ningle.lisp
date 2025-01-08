cbbc6c5cbc89124577576f90fbb056a0cfe5e0e4
NMunro <neilmunro@gmail.com>
2025-01-08 19:09:12 +0000

    Reset ningle csrf session token

--- src/ningle.lisp
+++ src/ningle.lisp
@@ -16,7 +16,8 @@
   (when (forms::form-csrf-protection-p form)
     ;; Check the csrf token
     (let ((session-token (get-form-session-csrf-token form)))
-      (when (or (not session-token) (not (equalp session-token (cdr (assoc (form-csrf-field-name form) (request-post-parameters request) :test #'string=)))))
+      (when (or (not session-token)
+                (not (equalp session-token (cdr (assoc (form-csrf-field-name form) (request-post-parameters request) :test #'string=)))))
         ;; The form is not valid. Throw an error, but reset its CSRF token for next time
         (set-form-session-csrf-token form)
         (error "Invalid CSRF token"))))
@@ -33,7 +34,5 @@
   (lack/middleware/csrf:csrf-token ningle:*session*))
 
 (defmethod set-form-session-csrf-token ((form form))
-  ;; (hunchentoot:start-session)
-  ;; (setf (hunchentoot:session-value (form-session-csrf-entry form))
-  ;;       (make-csrf-token form)))
-  nil)
+  (remhash lack/middleware/csrf::*csrf-session-key* ningle:*session*)
+  (lack/middleware/csrf:csrf-token ningle:*session*))
